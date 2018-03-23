7e402aa2d3505dd6dc0c9e922315a12092427f21
Chad Underhill <chad.j.underhill@gmail.com>
2018-03-23 01:32:22 +0100

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -4,4 +4,4 @@ virtualenv /home/ec2-user/www/apache_backend-venv
 sudo chown ec2-user:ec2-user /home/ec2-user/www/apache_backend-venv
 sudo chown ec2-user:ec2-user /home/ec2-user/www/apache_backend-venv/*
 source /home/ec2-user/www/apache_backend-venv/bin/activate
-pip install -r /home/ec2-user/www/apache_backend/requirements.txt
+pip install Django djangorestframework django-cors-headers django-filter djangorestframework-filters Pillow
