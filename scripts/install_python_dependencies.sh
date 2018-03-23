597fe491b2e8e7aeafc61f3f5b1a6961a99cfab0
Chad Underhill <chad.j.underhill@gmail.com>
2018-03-23 01:18:22 +0100

    Update install_python_dependencies.sh

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -4,4 +4,4 @@ virtualenv /home/ec2-user/www/apache_backend-venv
 sudo chown ec2-user:ec2-user /home/ec2-user/www/apache_backend-venv
 sudo chown ec2-user:ec2-user /home/ec2-user/www/apache_backend-venv/*
 source /home/ec2-user/www/apache_backend-venv/bin/activate
-pip3 install -r /home/ec2-user/www/apache_backend/apache/requirements.txt
+pip install -r /home/ec2-user/www/apache_backend/apache/requirements.txt
