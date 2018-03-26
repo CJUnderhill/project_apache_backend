671a1ec9a041d507545a31481d46e32e5bea4a8a
root <root@DESKTOP-QEECBS3.localdomain>
2018-03-26 19:57:24 +0200

    npm why

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -6,5 +6,6 @@ apt-get -y install ruby
 apt-get -y install wget
 apt-get -y install python3-pip
 pip3 install --upgrade pip3
-apt-get -y install nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
 npm i -g npm
