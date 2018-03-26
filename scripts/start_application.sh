c1c3801cb39b14c9a67fae079bd67de8307eece5
Chad Underhill <chad.j.underhill@gmail.com>
2018-03-26 15:01:22 +0200

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/www/apache_backend/
 source /home/ubuntu/www/apache_backend-venv/bin/activate
-python3 ./manage.py runserver
+nohup python3 ./manage.py runserver &
