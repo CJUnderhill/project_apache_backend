6d3a4f3753a852f569606455ccbd40715a1c6fc4
root <root@DESKTOP-QEECBS3.localdomain>
2018-03-26 14:51:22 +0200

    Updated more stuff for use with ubuntu

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo chown ec2-user:ec2-user /home/ec2-user/www
-virtualenv /home/ec2-user/www/apache_backend-venv
-sudo chown ec2-user:ec2-user /home/ec2-user/www/apache_backend-venv
-sudo chown ec2-user:ec2-user /home/ec2-user/www/apache_backend-venv/*
-source /home/ec2-user/www/apache_backend-venv/bin/activate
-pip3 install -r /home/ec2-user/www/apache_backend/requirements.txt
+sudo chown ubuntu:ubuntu /home/ubuntu/www
+virtualenv /home/ubuntu/www/apache_backend-venv
+sudo chown ubuntu:ubuntu /home/ubuntu/www/apache_backend-venv
+sudo chown ubuntu:ubuntu /home/ubuntu/www/apache_backend-venv/*
+source /home/ubuntu/www/apache_backend-venv/bin/activate
+pip3 install Django djangorestframework django-cors-headers django-filter djangorestframework-filters Pillow
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/www/apache_backend/
-source /home/ec2-user/www/apache_backend-venv/bin/activate
+cd /home/ubuntu/www/apache_backend/
+source /home/ubuntu/www/apache_backend-venv/bin/activate
 python3 ./manage.py runserver
