26ba7f61e954687a00089496bf3b0a3535928659
root <root@DESKTOP-QEECBS3.localdomain>
2018-03-27 10:49:16 +0200

    finally runs

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec python3 ./manage.py runserver 0.0.0.0:8000
+python3 ./manage.py runserver 0.0.0.0:8000
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/www/apache_backend/
 source /home/ubuntu/www/apache_backend-venv/bin/activate
-forever start scripts/runserver.sh
+forever start -c /bin/bash scripts/runserver.sh
