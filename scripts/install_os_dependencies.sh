1290ec0467daaedf6818b7c156f74b0a1981640f
cjunderhill <cjunderhill@wpi.edu>
2018-04-03 22:43:40 +0200

    why

--- scripts/install_os_dependencies.sh
+++ scripts/install_os_dependencies.sh
@@ -6,6 +6,7 @@ apt-get -y install ruby
 apt-get -y install wget
 apt-get -y install python3-pip
 pip3 install --upgrade pip3
+pip3 install --upgrade virtualenv
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm i -g npm
